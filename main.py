0793922e49338696204eab143f0febaae8111d50
Ankai Jie <ankaijie@gmail.com>
2017-03-04 00:02:35 -0500

    start strat

--- main.py
+++ main.py
@@ -4,14 +4,38 @@ import threading
 r = MyReponse()
 
 def main():
-	updateThread = threading.Thread(target = statusUpdate)
-	updateThread.start()
+    updateThread = threading.Thread(target = statusUpdate)
+    updateThread.start()
+
+
+def strategy():
+    print(r.config)
+    print(moveBoundaries())
+
+    moveType = moveToCorner()
+
+
+def moveToCorner():
+    
+
+
+def moveBoundaries():
+    width = r.config['mapwidth']
+    height = r.config['mapheight']
+    xrate = 0
+    yrate = 0.5
+    xlen = 0.5
+    ylen = 0.5
+
+    return (xrate * width, yrate*height, xlen * width, ylen*height)
+
+
 
 
 def statusUpdate():
-	while True:
-		time.sleep(0.1)
-		r.update()
-		print(r)
+    while True:
+        time.sleep(0.1)
+        r.update()
+        print(r)
 
-main()
+strategy()
