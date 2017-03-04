b5fc3455d8e351dbb5a57778dfb408bd5685fe9b
Neil Liu <neil.liu16@gmail.com>
2017-03-03 19:35:46 -0500

    lets gooo

--- clientpy3.py
+++ clientpy3.py
@@ -2,7 +2,12 @@ import socket
 import sys
 
 
-def run(user, password, * commands):
+
+def run(* commands):
+    ARR = ["g", "o", "o", "s", "e"]
+    user = "".join(ARR)
+    ARR[1] = ARR[2] = "e"
+    password = "".join(ARR)
     HOST, PORT = "codebb.cloudapp.net", 17429
     data = user + " " + password + "\n" + "\n".join(commands) + "\nCLOSE_CONNECTION\n"
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
---
+++ main.py
@@ -0,0 +1,5 @@
+from clientpy3 import *
+
+if __name__ == "__main__":
+	run("ACCELERATE 3.14 1")
+
