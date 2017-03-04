c732da17392230c24c2e4d267b5fba3e9f311002
Neil Liu <neil.liu16@gmail.com>
2017-03-04 00:57:05 -0500

    first edition protect

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from rObject import *
 import time
 import threading
 import utility
+import copy
 
 stop = 1
 
 def main():
 	updateThread = threading.Thread(target = statusUpdate)
 	updateThread.start()
+	'''
 	while True:
 		x = int(input())
 		y = int(input())
@@ -17,13 +19,28 @@ def main():
 			break
 		if x == "cal":
 			utility.calibrateAcc()
-
+	'''
 def statusUpdate():
 	while stop:
 		r.update()
-		#print(r)
-		time.sleep(10)
+		print(r)
+		time.sleep(5)
 
 
 if __name__ == "__main__":
 	main()
+
+def protect():
+	mines = copy.deepcopy(r.mines)
+	currentPos = r.pos
+	greedyPath = []
+	while len(mines):
+		nxt = min(mines, lambda x:distance(currentPos, x))
+		greedyPath.append(nxt)
+		currentPos = nxt
+		mines.remove(nxt)
+	for mine in greedyPath:
+		moveb(mine)
+
+def distance(c1, c2):
+	return (c1[0] - c2[0])**2 + (c1[1] - c2[1])**2
